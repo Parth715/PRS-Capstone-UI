fc04ecf7aedd2e74379b36ab661bf9ce40982d8a
Parth Patel <parth2002715@gmail.com>
2021-11-23 10:57:44 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { RequestsComponent } from './requests/requests.component';
+import { UsersComponent } from './users/users.component';
+import { VendorsComponent } from './vendors/vendors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "", pathMatch: "full"},
+  {path: "user", component: UsersComponent},
+  {path: "product", component: ProductsComponent},
+  {path: "request", component: RequestsComponent},
+  {path: "vendor", component: VendorsComponent},
+  {path: "**", component: UsersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { RequestsComponent } from './requests/requests.component';
+import { UsersComponent } from './users/users.component';
+import { VendorsComponent } from './vendors/vendors.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    RequestsComponent,
+    UsersComponent,
+    VendorsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
