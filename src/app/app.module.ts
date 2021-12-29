b4c2afd69328a06999ebef803b3f72269d2a41f8
Parth Patel <parth2002715@gmail.com>
2021-12-28 20:12:31 -0500

    Pulled form git hub and had to make some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -57,8 +57,6 @@ import { Review1Component } from './review/review1/review1.component';
     RequestlinesEditComponent,
     RequestlinesCreateComponent,
     Review1Component,
-  
-    
   ],
   imports: [
     BrowserModule,
